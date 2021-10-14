fc0852f58ababa8e20229f2f48e5a8df9f1b3c43
youngmoon525 <77032646+youngmoon525@users.noreply.github.com>
2021-10-14 14:33:26 +0900

    255

--- app/src/main/java/com/alcohol/finalalcohol/StartActivity.java
+++ app/src/main/java/com/alcohol/finalalcohol/StartActivity.java
@@ -2,28 +2,16 @@ package com.alcohol.finalalcohol;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.Activity;
 import android.os.Bundle;
-
-import com.kakao.kakaolink.KakaoLink;
-import com.kakao.message.template.FeedTemplate;
-import com.kakao.util.KakaoParameterException;
-
 
 public class StartActivity extends AppCompatActivity {
 
     //TextView textView;
-    public KakaoLink kakaoLink;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
-
-        try {
-            kakaoLink = KakaoLink.getKakaoLink(StartActivity.this);
-        } catch (KakaoParameterException e) {
-            e.printStackTrace();
-        }
 
         //Hello hello = new Hello();
 
@@ -31,5 +19,4 @@ public class StartActivity extends AppCompatActivity {
         //textView.setText(hello.formatMessage("Android with kotlin"));
 
     }
-
 }
--- app/src/main/java/com/alcohol/finalalcohol/bottomnavi/FragmentSubs.java
+++ app/src/main/java/com/alcohol/finalalcohol/bottomnavi/FragmentSubs.java
@@ -1,16 +1,17 @@
-package com.alcohol.finalalcohol.bottomnavi;
+package com.alcohol.finalalcohol.Bottomnavi;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.alcohol.finalalcohol.R;
+import com.alcohol.finalalcohol.Subscribe.Subs_input;
 
 public class FragmentSubs extends Fragment {
 
@@ -19,8 +20,14 @@ public class FragmentSubs extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_subs, container, false);
 
-
-
+        rootView.findViewById(R.id.btn_subs).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), Subs_input.class);
+                startActivity(intent);
+            }
+        });
+        
         return rootView;
     }
 }
