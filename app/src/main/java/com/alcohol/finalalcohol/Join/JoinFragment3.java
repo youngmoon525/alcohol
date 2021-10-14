aecf13df57857b09c76beb83e10d3de1e20aaea7
youngmoon525 <77032646+youngmoon525@users.noreply.github.com>
2021-10-14 15:14:44 +0900

    yynn

--- app/src/main/java/com/alcohol/finalalcohol/Join/JoinFragment3.java
+++ app/src/main/java/com/alcohol/finalalcohol/Join/JoinFragment3.java
@@ -3,33 +3,26 @@ package com.alcohol.finalalcohol.Join;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
-
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.util.Log;
-import android.util.Patterns;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import com.alcohol.finalalcohol.ATask.JoinInsert;
-import com.alcohol.finalalcohol.MainActivity;
+import com.alcohol.finalalcohol.LoginActivity;
 import com.alcohol.finalalcohol.R;
 
 import java.io.File;
-import java.util.regex.Pattern;
+import java.util.concurrent.ExecutionException;
 
 public class JoinFragment3 extends Fragment {
 
@@ -40,23 +33,39 @@ public class JoinFragment3 extends Fragment {
     String imgFilePath = "";
 
     String state;
-    RadioGroup Group_body;
-    RadioGroup Group_alcohol_type;
-    RadioGroup Group_smell;
-    RadioGroup Group_alcohol_bv;
     RadioGroup Group_flavor;
+    RadioGroup Group_alcohol_bv;
+    RadioGroup Group_smell;
+    RadioGroup Group_alcohol_type;
+    RadioGroup Group_body;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_join1, container, false);
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment_join3, container, false);
 
-        Group_body = (RadioGroup)rootView.findViewById(R.id.Group_body);
-        Group_alcohol_type = (RadioGroup)rootView.findViewById(R.id.Group_alcohol_type);
-        Group_smell = (RadioGroup)rootView.findViewById(R.id.Group_smell);
-        Group_alcohol_bv = (RadioGroup)rootView.findViewById(R.id.Group_alcohol_bv);
+        String mem_email = getArguments().getString("mem_email"); // 프래그먼트1에서 받아온 값 넣기
+        String mem_nickname = getArguments().getString("mem_nickname"); // 프래그먼트1에서 받아온 값 넣기
+        String mem_pw = getArguments().getString("mem_pw"); // 프래그먼트1에서 받아온 값 넣기
+        String mem_address = getArguments().getString("mem_address"); // 프래그먼트1에서 받아온 값 넣기
+        Log.d(TAG, "onCreateView: " + mem_address);
+        String mem_gender = "";
+        if(getArguments().getString("mem_gender").equals("남")){
+            mem_gender = "M";
+        }else if (getArguments().getString("mem_gender").equals("여")){
+            mem_gender = "F";
+        }
+        String mem_name = getArguments().getString("mem_name"); // 프래그먼트1에서 받아온 값 넣기
+        String mem_birth = getArguments().getString("mem_birth"); // 프래그먼트1에서 받아온 값 넣기
+        String mem_phone = getArguments().getString("mem_phone"); // 프래그먼트1에서 받아온 값 넣기
+        //Log.d(TAG, ""+ mem_email + ", " + mem_nickname + ", " + mem_pw + ", " + mem_gender + ", "
+        //        + mem_address + ", " + mem_name + ", " + mem_birth + ", " + mem_phone);
+
         Group_flavor = (RadioGroup)rootView.findViewById(R.id.Group_flavor);
-
+        Group_alcohol_bv = (RadioGroup)rootView.findViewById(R.id.Group_alcohol_bv);
+        Group_smell = (RadioGroup)rootView.findViewById(R.id.Group_smell);
+        Group_alcohol_type = (RadioGroup)rootView.findViewById(R.id.Group_alcohol_type);
+        Group_body = (RadioGroup)rootView.findViewById(R.id.Group_body);
 
         // 체크박스와 달리 라디오 그룹은 그룹 전체를 등록한다
         // RadioButton 은 각각 선언하는 것이 아니라 RadioGroup 으로 선언해서 사용한다.
@@ -76,7 +85,7 @@ public class JoinFragment3 extends Fragment {
                 builder.setPositiveButton("회원가입취소", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        Intent intent = new Intent(getContext(), MainActivity.class);
+                        Intent intent = new Intent(getContext(), LoginActivity.class);
                         startActivity(intent);
                     }
                 });//회원가입취소
@@ -95,9 +104,106 @@ public class JoinFragment3 extends Fragment {
         });//btnExit
 
         //회원가입
+        String finalMem_gender = mem_gender;
         rootView.findViewById(R.id.btnJoin).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                int mem_flavor_int = Group_flavor.getCheckedRadioButtonId();
+                RadioButton buttonFlavor = (RadioButton) rootView.findViewById(mem_flavor_int);
+                String mem_flavor_str = buttonFlavor.getText().toString();
+                //Log.d(TAG, "onClick: " + mem_flavor_str);
+                int mem_flavor = 0;
+
+                if(mem_flavor_str.equals("sweet")){
+                    mem_flavor = 9;
+                } else if(mem_flavor_str.equals("acidity")){
+                    mem_flavor = 10;
+                } else if(mem_flavor_str.equals("bitter")){
+                    mem_flavor = 11;
+                } else if(mem_flavor_str.equals("salty")){
+                    mem_flavor = 12;
+                } else if(mem_flavor_str.equals("spicy")){
+                    mem_flavor = 13;
+                }
+                //RadioButton flavorButton = (RadioButton) rootView.findViewById(mem_flavor);
+
+                int mem_alcohol_bv_int = Group_alcohol_bv.getCheckedRadioButtonId();
+                RadioButton buttonAlcoholBv = (RadioButton) rootView.findViewById(mem_alcohol_bv_int);
+                String mem_alcohol_bv_str = buttonAlcoholBv.getText().toString();
+                int mem_alcohol_bv = 0;
+
+                if(mem_alcohol_bv_str.equals("1~10")){
+                    mem_alcohol_bv = 18;
+                } else if(mem_alcohol_bv_str.equals("11~20")){
+                    mem_alcohol_bv = 19;
+                } else if(mem_alcohol_bv_str.equals("21~30")){
+                    mem_alcohol_bv = 20;
+                } else if(mem_alcohol_bv_str.equals("31~40")){
+                    mem_alcohol_bv = 21;
+                }
+                //RadioButton bvButton = (RadioButton) rootView.findViewById(mem_alcohol_bv);
+
+                int mem_smell_int = Group_smell.getCheckedRadioButtonId();
+                RadioButton buttonSmell = (RadioButton) rootView.findViewById(mem_smell_int);
+                String mem_smell_str = buttonSmell.getText().toString();
+                int mem_smell = 0;
+
+                if(mem_smell_str.equals("fruit")){
+                    mem_smell = 14;
+                } else if(mem_smell_str.equals("root")){
+                    mem_smell = 15;
+                } else if(mem_smell_str.equals("grain")){
+                    mem_smell = 16;
+                } else if(mem_smell_str.equals("plant")){
+                    mem_smell = 17;
+                }
+                //RadioButton smellButton = (RadioButton) rootView.findViewById(mem_smell);
+
+                int mem_alcohol_type_int = Group_alcohol_type.getCheckedRadioButtonId();
+                RadioButton buttonAlcoholType = (RadioButton) rootView.findViewById(mem_alcohol_type_int);
+                String mem_alcohol_type_str = buttonAlcoholType.getText().toString();
+                int mem_alcohol_type = 0;
+
+                if(mem_alcohol_type_str.equals("takju")){
+                    mem_alcohol_type = 4;
+                } else if(mem_alcohol_type_str.equals("lijueur")){
+                    mem_alcohol_type = 5;
+                } else if(mem_alcohol_type_str.equals("yakju")){
+                    mem_alcohol_type = 6;
+                } else if(mem_alcohol_type_str.equals("soju")){
+                    mem_alcohol_type = 7;
+                }else if(mem_alcohol_type_str.equals("fruitwine")){
+                    mem_alcohol_type = 8;
+                }
+                //RadioButton typeButton = (RadioButton) rootView.findViewById(mem_type);
+
+                int mem_body_int = Group_body.getCheckedRadioButtonId();
+                RadioButton buttonBody = (RadioButton) rootView.findViewById(mem_body_int);
+                String mem_body_str = buttonBody.getText().toString();
+                int mem_body = 0;
+
+                if(mem_body_str.equals("LightBody")){
+                    mem_body = 1;
+                } else if(mem_body_str.equals("MidiumBody")){
+                    mem_body = 2;
+                } else if(mem_body_str.equals("FullBody")){
+                    mem_body = 3;
+                }
+                //RadioButton bodyButton = (RadioButton) rootView.findViewById(mem_body);
+
+                Log.d(TAG, ""+ mem_flavor +","+mem_alcohol_bv+","+mem_smell+","+mem_alcohol_type+","+mem_body+",,,,,"+ finalMem_gender);
+
+                /*
+                String mem_flavor = flavorButton.getText().toString();
+                String mem_alcohol_bv = bvButton.getText().toString();
+                String mem_smell = smellButton.getText().toString();
+                String mem_alcohol_type = typeButton.getText().toString();
+                String mem_body = bodyButton.getText().toString();
+                 */
+
+                //Log.d(TAG, ""+ mem_flavor + ", " + mem_alcohol_bv + ", " + mem_smell + ", " + mem_alcohol_type + ", "
+                //        + mem_body);
 
                 // 1. builder 선언
                 AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
@@ -106,75 +212,35 @@ public class JoinFragment3 extends Fragment {
                 //3. 알림창 내용
                 builder.setMessage("회원가입을 진행하시겠습니까?");
                 // 3. 알림창에 띄울 요소
+                int finalMem_flavor = mem_flavor;
+                int finalMem_alcohol_bv = mem_alcohol_bv;
+                int finalMem_smell = mem_smell;
+                int finalMem_body = mem_body;
+                int finalMem_alcohol_type = mem_alcohol_type;
                 builder.setPositiveButton("회원가입", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
-                        //다음에 넘겨야함
 
-                        Group_body = (RadioGroup)rootView.findViewById(R.id.Group_body);
-                        Group_alcohol_type = (RadioGroup)rootView.findViewById(R.id.Group_alcohol_type);
-                        Group_smell = (RadioGroup)rootView.findViewById(R.id.Group_smell);
-                        Group_alcohol_bv = (RadioGroup)rootView.findViewById(R.id.Group_alcohol_bv);
-                        Group_flavor = (RadioGroup)rootView.findViewById(R.id.Group_flavor);
+                        String mem_post = "";
+                        int mem_id = 1;
 
-                        int id_Group_body = Group_body.getCheckedRadioButtonId();
-                        RadioButton rb_Group_body = (RadioButton)rootView.findViewById(id_Group_body);
-                        // 바디감 : rb_Group_body.getText().toString()
+                        JoinInsert joinInsert =
+                                new JoinInsert(mem_email, mem_pw , mem_name, mem_nickname, mem_birth
+                                        ,  mem_address, mem_post
+                                        , finalMem_gender, mem_phone, finalMem_body, finalMem_alcohol_type, finalMem_flavor
+                                        , finalMem_smell, finalMem_alcohol_bv);
 
-                        int id_Group_alcohol_type = Group_alcohol_type.getCheckedRadioButtonId();
-                        RadioButton rb_Group_alcohol_type = (RadioButton)rootView.findViewById(id_Group_alcohol_type);
-                        // 주종 : rb_Group_alcohol_type.getText().toString()
+                        try {
+                            state = joinInsert.execute().get();
+                        } catch (ExecutionException e){
+                            e.printStackTrace();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
 
-                        int id_Group_smell = Group_smell.getCheckedRadioButtonId();
-                        RadioButton rb_Group_smell = (RadioButton)rootView.findViewById(id_Group_smell);
-                        // 향 : rb_Group_smell.getText().toString()
-
-                        int id_Group_alcohol_bv = Group_alcohol_bv.getCheckedRadioButtonId();
-                        RadioButton rb_Group_alcohol_bv = (RadioButton)rootView.findViewById(id_Group_alcohol_bv);
-                        // 도수 : rb_Group_alcohol_bv.getText().toString()
-
-                        int id_Group_flavor = Group_flavor.getCheckedRadioButtonId();
-                        RadioButton rb_Group_flavor = (RadioButton)rootView.findViewById(id_Group_flavor);
-                        // 맛 : rb_Group_flavor.getText().toString()
-
-                        //JoinFragment3 fragment3 = new JoinFragment3();//프래그먼트3선언
-
-                        //앞 프래그먼트에서 값 가져온 것 넣기
-                        //name = getArguments().getString("name"); // 프래그먼트1에서 받아온 값 넣기
-
-                        String mem_email = getArguments().getString("mem_email");
-                        String mem_nickname = getArguments().getString("mem_nickname");
-                        String mem_pw = getArguments().getString("mem_pw");
-                        String mem_address = getArguments().getString("mem_address");
-                        String mem_gender = getArguments().getString("mem_gender");
-                        String mem_name = getArguments().getString("mem_name");
-                        String mem_birth = getArguments().getString("mem_birth");
-                        String mem_phone = getArguments().getString("mem_phone");
-
-                        //취향넣기
-                        String mem_body = rb_Group_body.getText().toString();
-                        String mem_alcohol_type = rb_Group_alcohol_type.getText().toString();
-                        String mem_smell= rb_Group_smell.getText().toString();
-                        String mem_alcohol_bv = rb_Group_alcohol_bv.getText().toString();
-                        String mem_flavor = rb_Group_flavor.getText().toString();
-
-                        Log.d(TAG, "mem_body:"+mem_body
-                                +"\nmem_alcohol_type:"+mem_alcohol_type
-                                +"\nmem_email"+mem_email);
-
-                        /*JoinInsert joinInsert = new JoinInsert(
-                                mem_email, mem_nickname, mem_pw, mem_address, mem_gender, mem_name, mem_birth, mem_phone,
-                                mem_body, mem_alcohol_type, mem_smell, mem_alcohol_bv, mem_flavor
-                                );
-
-                        FragmentTransaction transaction = getActivity().getSupportFragmentManager().beginTransaction();
-                        fragment2.setArguments(bundle);//번들을 프래그먼트2로 보낼 준비
-                        transaction.replace(R.id.frame, fragment2);
-                        transaction.commit();*/
-
-                        /*Intent intent = new Intent(getContext(), MainActivity.class);
-                        startActivity(intent);*/
-                        //Toast.makeText(getContext(), "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getContext(), LoginActivity.class);
+                        startActivity(intent);
+                        Toast.makeText(getContext(), "회원가입이 완료되었습니다.", Toast.LENGTH_SHORT).show();
                     }
                 });//회원가입이어서 진행
 
