8b47d1476aef927f42170b7019a165e40ebe5852
DESKTOP-ATSLSEN\lcocal <youngmoon525@naver.com>
2022-05-16 14:57:27 +0900

    완

--- app/src/main/java/com/alcohol/finalalcohol/MySubsDetail/mySubsDetailListener.java
+++
@@ -1,7 +0,0 @@
-package com.alcohol.finalalcohol.MySubsDetail;
-
-import android.view.View;
-
-public interface mySubsDetailListener {
-    public void onItemClick(mySubsDetailAdapter.ViewHolder holder, View view, int position);
-}
